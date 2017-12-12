1b9e49b661cc1aae896f6e569a8c9ceba3218956
CXB <2414196988@qq.com>
2017-12-12 13:46:25 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/com/chenxianbao/coolweather/WeatherActivity.java
+++ app/src/main/java/com/chenxianbao/coolweather/WeatherActivity.java
@@ -106,7 +106,7 @@ public class WeatherActivity extends AppCompatActivity {
             weatherLayout.setVisibility(View.INVISIBLE);
             requestWeather(mWeatherId);
         }
-        /**
+
         swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
@@ -118,7 +118,7 @@ public class WeatherActivity extends AppCompatActivity {
             public void onClick(View v) {
                 drawerLayout.openDrawer(GravityCompat.START);
             }
-        });*/
+        });
         String bingPic = prefs.getString("bing_pic", null);
         if (bingPic != null) {
             Glide.with(this).load(bingPic).into(bingPicImg);
